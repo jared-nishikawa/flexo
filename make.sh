409f091bb8dd5945f4b3bd24c44f2dd28a3c9d4e
Jared <jared.nishikawa@gmail.com>
2020-01-04 17:27:16 -0700

    Initial commit

---
+++ make.sh
@@ -0,0 +1,14 @@
+# needed sudo apt install mingw-w64
+BIN="bin"
+if [ -z $1 ]
+then
+    echo Need arg
+    exit 0
+fi
+if [ $1 == "clean" ]
+then
+    rm $BIN/*
+    exit
+fi
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -o $BIN/${1%.*}_x64.exe $1
+go build -o $BIN/${1%.*}_x64 $1
